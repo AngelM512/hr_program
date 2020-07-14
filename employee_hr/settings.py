402e258cf550777c11377b33bec060f54c562350
Angel Moreta <37674133+rundacode@users.noreply.github.com>
2020-07-14 06:14:02 -0500

    Update settings.py

--- employee_hr/settings.py
+++ employee_hr/settings.py
@@ -133,7 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-# EMAIL SETTINGS
+######## EMAIL CONFIGURATION ########
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
