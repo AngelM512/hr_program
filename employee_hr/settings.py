f8ff6f0080401c1a9c389124bcfd28c4bdd5ce30
Angel Moreta <37674133+AngelM512@users.noreply.github.com>
2022-04-24 12:33:47 -0500

    db setup

--- employee_hr/settings.py
+++ employee_hr/settings.py
@@ -78,12 +78,37 @@ WSGI_APPLICATION = 'employee_hr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
+
+
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': config('NAME'),
+
+        'USER': 'postgres',
+
+        'PASSWORD': config('PASSWORD'),
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
 
 
 # Password validation
@@ -138,5 +163,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+#EMAIL_HOST_USER = config('EMAIL_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
